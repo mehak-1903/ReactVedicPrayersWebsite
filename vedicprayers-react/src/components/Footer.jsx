1c939cb6b1cded6bc98e7ad403a26c889d334dee
mehak-1903 <mehakaggarwal1019@gmail.com>
2025-06-14 18:03:31 +0530

    Updating header and footer using tailwind css

--- vedicprayers-react/src/components/Footer.jsx
+++ vedicprayers-react/src/components/Footer.jsx
@@ -10,36 +10,37 @@ import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
   return (
-    
-      <div className="footer">
-        <div className="left">© 2025 All rights reserved</div>
-        <div className="center">
-          <a href="#">
-            <FontAwesomeIcon
-              icon={faFacebookF}
-            /></a>
-            <a href = "#"><FontAwesomeIcon
-              icon={faTwitter}
-            /></a>
-            <a href = "#"><FontAwesomeIcon
-              icon={faInstagram}
-            /></a>
-            <a href = "#"><FontAwesomeIcon
-              icon={faPinterest}
-            /></a>
-            <a href = "#"><FontAwesomeIcon
-              icon={faYoutube}
-            />
-          </a>
-
-        </div>
-        <div className="right">
-          <a href = "#">
-            <FontAwesomeIcon icon = {faAngleUp} style={{marginRight: '10px'}}/>
-          Back to Top</a></div>
+    <div className="flex justify-between items-center h-auto w-[1260px] m-auto border-t border-grey-400 py-7">
+      <p>© 2025 All rights reserved</p>
+      <div className="flex gap-5">
+        <a href="#" className="hover:text-orange-600">
+          <FontAwesomeIcon icon={faFacebookF} />
+        </a>
+        <a href="#" className="hover:text-orange-600">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a href="#" className="hover:text-orange-600">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a href="#" className="hover:text-orange-600">
+          <FontAwesomeIcon icon={faPinterest} />
+        </a>
+        <a href="#" className="hover:text-orange-600">
+          <FontAwesomeIcon icon={faYoutube} />
+        </a>
       </div>
-    
-    
+
+      <div className="border border-grey-400 px-3 py-2 rounded-3xl hover:border-orange-600">
+        <a href="#" className="hover:text-orange-600">
+          <FontAwesomeIcon
+            icon={faAngleUp}
+            style={{ marginRight: "10px" }}
+            className="text-grey"
+          />
+          Back to Top
+        </a>
+      </div>
+    </div>
   );
 };
 
--- vedicprayers-react/src/components/Header.jsx
+++ vedicprayers-react/src/components/Header.jsx
@@ -12,41 +12,79 @@ import "../app.css";
 
 export const Header = () => {
   return (
-    <div className="header">
-      <div className="header-container">
-        <div className="left">
-          <a href="#">
-            <FontAwesomeIcon icon={faFacebookF} />
-          </a>
-          <a href="#">
-            <FontAwesomeIcon icon={faTwitter} />
-          </a>
-          <a href="#">
-            <FontAwesomeIcon icon={faInstagram} />
-          </a>
-          <a href="#">
-            <FontAwesomeIcon icon={faPinterest} />
-          </a>
-          <a href="#">
-            <FontAwesomeIcon icon={faYoutube} />
-          </a>
-        </div>
-        <div className="center">
-          <img src={logo} alt="Vedic Logo" height="80px" width="80px" />
-          <a href="#">
-            VedicPrayers<span>.</span>
-          </a>
-          <span className="slogan">Ancient Vedic Mantras and Rituals</span>
-        </div>
-        <div className="right">
-          <button>
-            <FontAwesomeIcon icon={faSearch} />
-          </button>
+    // <div className="header">
+    //   <div className="header-container">
+    //     <div className="left">
+    //       <a href="#">
+    //         <FontAwesomeIcon icon={faFacebookF} />
+    //       </a>
+    //       <a href="#">
+    //         <FontAwesomeIcon icon={faTwitter} />
+    //       </a>
+    //       <a href="#">
+    //         <FontAwesomeIcon icon={faInstagram} />
+    //       </a>
+    //       <a href="#">
+    //         <FontAwesomeIcon icon={faPinterest} />
+    //       </a>
+    //       <a href="#">
+    //         <FontAwesomeIcon icon={faYoutube} />
+    //       </a>
+    //     </div>
+    //     <div className="center">
+    //       <img src={logo} alt="Vedic Logo" height="80px" width="80px" />
+    //       <a href="#">
+    //         VedicPrayers<span>.</span>
+    //       </a>
+    //       <span>Ancient Vedic Mantras and Rituals</span>
+    //     </div>
+    //     <div className="right">
+    //       <button>
+    //         <FontAwesomeIcon icon={faSearch} />
+    //       </button>
 
-          <button>
-            <FontAwesomeIcon icon={faBars} />
-          </button>
-        </div>
+    //       <button>
+    //         <FontAwesomeIcon icon={faBars} />
+    //       </button>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div className="flex justify-between items-center h-auto w-[1260px] m-auto border-b border-grey-400 py-7">
+      <div className="flex gap-5">
+        <a href="#" className="hover:text-orange-600">
+          <FontAwesomeIcon icon={faFacebookF} />
+        </a>
+        <a href="#" className="hover:text-orange-600">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a href="#" className="hover:text-orange-600">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a href="#" className="hover:text-orange-600">
+          <FontAwesomeIcon icon={faPinterest} />
+        </a>
+        <a href="#" className="hover:text-orange-600">
+          <FontAwesomeIcon icon={faYoutube} />
+        </a>
+      </div>
+
+      <div className="flex flex-col items-center text-center mb-4">
+        <img src={logo} alt="Vedic Logo" height="80px" width="80px"/>
+        <a href="#" className="text-4xl font-bold">
+          VedicPrayers<span className="text-orange-600 text-6xl">.</span>
+        </a>
+        <span>Ancient Vedic Mantras and Rituals</span>
+      </div>
+
+      <div className=" flex gap-3 ">
+        <button className="h-[30] w-[30] rounded-full border-none bg-orange-600 py-2 px-3 text-white">
+          <FontAwesomeIcon icon={faSearch} />
+        </button>
+
+        <button className="h-[30] w-[30] rounded-full border-none bg-orange-600 py-2 px-3 text-white">
+          <FontAwesomeIcon icon={faBars} />
+        </button>
       </div>
     </div>
   );
