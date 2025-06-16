153d9945d3436583596d2f362fdbcebce6e8fd2a
mehak-1903 <mehakaggarwal1019@gmail.com>
2025-06-16 17:00:27 +0530

    Adding dropdown list

--- vedicprayers-react/src/components/Card.jsx
+++ vedicprayers-react/src/components/Card.jsx
@@ -5,19 +5,58 @@ import img2 from "../assets/Shri-Hanuman-Vrat-Katha.jpeg";
 import img3 from "../assets/hanuman-stuti.jpg";
 import img4 from "../assets/Shri-Hanuman-Mantras.jpeg";
 import img5 from "../assets/hanuman-aarti.jpeg";
-
+import Flag from "react-world-flags";
 const navli = [
   {
     name: "Shri Ganesh Ji",
+    submenu: [
+      "Shri Ganesh Visarjan",
+      "Ganesh Chaturthi Vrat Katha",
+      "Shri Ganesh Stotra",
+      "Shri Ganesh Ji 108 Names",
+      "Shri Ganesh Chalisa",
+      "Shri Ganesh Ji Aarti",
+    ],
   },
   {
     name: "Lord Shiva",
+    submenu: [
+      "Shiv Ji Tandav Stotra",
+      "Shiv Ji Panchakshar Stotra",
+      "Shiv Ji Aarti",
+      "Shiv Ji Chalisa",
+      "Lord Shiv Ji Vrat Katha",
+      "Shiv Ji 108 Names",
+    ],
   },
   {
     name: "Lord Hanuman",
+    submenu: [
+      "Shri Hanuman Chalisa",
+      "Shri Hanuman Bajrang Baan",
+      "Shri Hanuman Sathika",
+      "Shri Hanuman Ashthak",
+      "Shri Hanuman Bahuk",
+      "Rinmochan Mangal Stotra",
+      "Shri Hanuman Vrat Katha",
+      "Shri Hanuman Mantras",
+      "Shri Hanuman Ji Aarti",
+      "Shri Hanuman Ji 108 Names",
+    ],
   },
   {
     name: "9 Goddess",
+    submenu: [
+      "Maa Shailputri",
+      "Maa Brahmacharini",
+      "Maa Chandraghanta",
+      "Maa Kushmanda",
+      "Maa Skandmata",
+      "Maa Katyayani",
+      "Maa Kalratri",
+      "Maa Mahagauri",
+      "Maa Siddhidatri",
+    ],
   },
 ];
 
@@ -37,26 +76,31 @@ const navli2 = [
 ];
 
 const bgImage = [
-  { image: img1 ,
+  {
+    image: img1,
     names: "Lord Hanuman Vrat | हनुमान व्रत | PDF",
-    badge: "Vrat"
+    badge: "Vrat",
   },
-  { image: img2 ,
+  {
+    image: img2,
     names: "Lord Hanuman Vrat Katha | हनुमान व्रत कथा | PDF",
-    badge: "Vrat Katha"
+    badge: "Vrat Katha",
   },
-  { image: img3,
+  {
+    image: img3,
     names: "Shri BajrangBali Stuti | श्री बजरंगबली स्तुति | PDF",
-    badge: "Stuti"
-   },
-  { image: img4,
+    badge: "Stuti",
+  },
+  {
+    image: img4,
     names: "Shri Hanuman 108 Names | श्री हनुमान 108 नाम | PDF",
-    badge: "God/Goddess Names"
-   },
-  { image: img5,
+    badge: "God/Goddess Names",
+  },
+  {
+    image: img5,
     names: "Shri Hanuman Ji Aarti | श्री हनुमान जी आरती | PDF",
-    badge: "Aarti"
-   },
+    badge: "Aarti",
+  },
 ];
 
 export default function Card() {
@@ -66,33 +110,83 @@ export default function Card() {
         <div className="flex justify-center items-center w-full">
           <ul className="flex justify-center items-center w-full gap-9 text-semibold">
             {navli.map((list, index) => (
-              <li className="text-lg">
+              <li className="relative group text-lg">
                 <a href="#" key={`nav1-${index}`}>
-                  {list.name}{" "}
+                  {list.name}
                   <FontAwesomeIcon
                     icon={faAngleDown}
-                    className="text-gray-400 text-sm"
+                    className="text-gray-400 text-sm pl-1"
                   />
                 </a>
+
+                {/* Dropdown Lists */}
+                {list.submenu && (
+                  <ul className="absolute top-full -left-6 mt-2 w-[280px] h-30 mb-6 bg-white rounded-md shadow-md opacity-0 group-hover:opacity-100 group-hover:translate-x-0 translate-y-2 transition-all duration-300 z-50 invisible group-hover:visible">
+                    <li>
+                      {list.submenu.map((item, subIndex) => (
+                        <li key={`sub-${index}-${subIndex}`} className="text-black hover:pl-7 hover:text-gray-400 transition-all duration-300">
+                          <a
+                            href="#"
+                            className="flex items-center gap-1 pl-5 text-left py-3"
+                          >
+                            {item}
+                          </a>
+                          <div className="border-b border-gray-200 w-20"></div>
+                        </li>
+                      ))}
+                    </li>
+                  </ul>
+                )}
               </li>
             ))}
 
             {navli2.map((list, index) => (
               <li className="text-lg">
-                <a href="#" key={`nav2-${index}`}>
+                <a
+                  href="#"
+                  key={`nav2-${index}`}
+                  className="flex items-center gap-1 pl-5 text-left py-3"
+                >
                   {list.navname}
                 </a>
               </li>
             ))}
 
-            <li className="text-lg">
-              <a href="#">
-                English{" "}
+            <li className="relative group">
+              <a
+                href="#"
+                className="text-lg flex justify-center items-center gap-1"
+              >
+                <Flag code="IN" style={{ width: 20, height: 15 }} />
+                हिंदी
                 <FontAwesomeIcon
                   icon={faAngleDown}
                   className="text-gray-400 text-sm"
                 />
               </a>
+
+              {/* Dropdown Menu */}
+              <ul className="absolute top-full left-0 mt-2 w-40 h-30 mb-3  bg-white rounded-md shadow-md opacity-0 group-hover:opacity-100 group-hover:translate-x-0 translate-y-2 transition-all duration-300 z-50 invisible group-hover:visible">
+                <li className="text-black hover:pl-7 hover:text-gray-300 transition-all duration-300">
+                  <a
+                    href="#"
+                    className="flex items-center gap-1 pl-5 text-left py-3"
+                  >
+                    <Flag code="US" style={{ width: 20, height: 15 }} />
+                    English
+                  </a>
+                </li>
+                <div className="w-1/4 h-px bg-gray-200"></div>
+                <li className="text-black hover:pl-7 hover:text-gray-300 transition-all duration-300">
+                  <a
+                    href="#"
+                    className="flex items-center gap-1 pl-5 text-left py-3"
+                  >
+                    <Flag code="IN" style={{ width: 20, height: 15 }} />
+                    हिंदी
+                  </a>
+                </li>
+              </ul>
             </li>
           </ul>
         </div>
@@ -119,20 +213,17 @@ export default function Card() {
             <div className="absolute inset-0 opacity-80 bg-[#17243b] rounded-2xl z-10">
               <div className="absolute top-10 z-20 flex flex-col items-center justify-center h-full px-6 mt-4">
                 <span className="px-3 py-1 w-auto rounded-2xl text-white mb-2 bg-[linear-gradient(to_right,#e45229_0%,#e99176_51%,#e45229_100%)]">
-                  <a href="#">
-                    {images.badge}
-                  </a>
-                  </span>
-                  <h4 className="text-center" key={`name-${index}`}>
-                    <a
-                      href="#"
-                      className="text-[22px] text-white font-bold leading-snug hover:text-orange-600"
-                    >
+                  <a href="#">{images.badge}</a>
+                </span>
+                <h4 className="text-center" key={`name-${index}`}>
+                  <a
+                    href="#"
+                    className="text-[22px] text-white font-bold leading-snug hover:text-orange-600"
+                  >
                     {images.names}
-                    </a>
-                  </h4>
+                  </a>
+                </h4>
               </div>
-             
             </div>
           </div>
         ))}
